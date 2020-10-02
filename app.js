ebb8387015780d93862b3dad76ddbe5804c58963
Chiro <sachinsinghroxx@gmail.com>
2020-10-02 23:29:19 +0530

    added node server and arranged the front end files for hosting

---
+++ app.js
@@ -0,0 +1,27 @@
+// loading all the modules
+
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("Website.html");
+});
+
+app.get("/landing",(req,res)=>{
+    res.render("index.html");
+});
+
+app.get("/Traffic",(req,res)=>{
+    res.render("Traffic.html");
+})
+
+app.listen(3000,()=>{
+    console.log("Server started at port 3000");
+});
